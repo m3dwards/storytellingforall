377774b2d1be99bf6184bfa2d6d1a1a1435852bb
Rhys <rhys@rhysjenkins.uk>
2024-07-21 16:59:12 +0100

    Mobile updates

--- _includes/javascript/nav.js
+++ _includes/javascript/nav.js
@@ -1,12 +1,14 @@
 const hamburger = document.querySelector("[data-hamburger-menu]");
 const mobileMenu = document.querySelector("[data-mobile-menu]");
+const landingText = document.querySelector(".intro");
 const overlay = document.querySelector("[data-overlay]");
 let menuIsOpen = false;
 
-if (hamburger && mobileMenu && overlay) {
+if (hamburger && mobileMenu && overlay && landingText) {
     function openMobileMenu() {
         hamburger.classList.add("open");
         mobileMenu.classList.remove("not-visible");
+        landingText.classList.add("not-visible");
         overlay.classList.add("visible");
         menuIsOpen = true;
     }
@@ -14,6 +16,7 @@ if (hamburger && mobileMenu && overlay) {
     function closeMobileMenu() {
         hamburger.classList.remove("open");
         mobileMenu.classList.add("not-visible");
+        landingText.classList.remove("not-visible");
         overlay.classList.remove("visible");
         menuIsOpen = false;
     }
